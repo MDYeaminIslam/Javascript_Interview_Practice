2dc43680c51075972108021bea7c454140ba1d35
Md. Yeamin Islam <yeamin15-13529@diu.edu.bd>
2023-12-09 23:47:12 +0600

    New file added

---
+++ Chai_aur_code_BackEnd/first_project/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+  res.send('Hello World!')
+});
+
+app.get('/twitter', (req,res) =>{
+  res.send('done.');
+});
+app.listen(port, () =>{
+  console.log(`App is listening at ${port}`);
+});
